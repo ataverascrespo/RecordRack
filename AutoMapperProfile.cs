b0dab8e4092d646bc8588559552d9a4578ce16e1
Alex Taveras-Crespo <alexmanuelt@hotmail.com>
2022-12-16 12:03:46 -0500

    Adds AutoMapper profiling between album and DTO

---
+++ AutoMapperProfile.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Configures mapping profiles for NuGet AutoMapper
+/// </summary>
+
+namespace AlbumAPI
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            //Create a mapping profile for Album to AddAlbumDTO
+            CreateMap<Album, GetAlbumDTO>();
+            //Create a mapping profile for AddAlbumDTO to Album
+            CreateMap<AddAlbumDTO, Album>();
+
+        }
+    }
+}
