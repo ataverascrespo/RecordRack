a932a1c383ac5b9973836ef994ee9958b26f5867
Alex Taveras-Crespo <alexmanuelt@hotmail.com>
2022-12-16 11:54:10 -0500

    Configure NuGet AutoMapper extension

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 global using AlbumAPI.Models;
 global using AlbumAPI.Services.AlbumServices;
 global using AlbumAPI.DTOs.Album;
+global using AutoMapper;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Implements NuGet AutoMapper service
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
+
 //Create a new instance of the requested album services 
 builder.Services.AddScoped<IAlbumService, AlbumService>();
 
