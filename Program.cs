6f6298e726db162b97d069a12ebced3a4e1df7e5
Alex Taveras-Crespo <alexmanuelt@hotmail.com>
2022-12-18 00:05:28 -0500

    Configure database connection string for EF

--- Program.cs
+++ Program.cs
@@ -3,11 +3,15 @@ global using AlbumAPI.Services.AlbumServices;
 global using AlbumAPI.DTOs.Album;
 global using AutoMapper;
 global using Microsoft.EntityFrameworkCore;
+global using AlbumAPI.Data;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
