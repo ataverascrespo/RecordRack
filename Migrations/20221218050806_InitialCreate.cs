bec16581306249ca68951c796f580f17abede8df
Alex Taveras-Crespo <alexmanuelt@hotmail.com>
2022-12-18 00:19:14 -0500

    Migrates model to database with Entity Framework

---
+++ Migrations/20221218050806_InitialCreate.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AlbumAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            //Specified table name from DataContext.cs
+            migrationBuilder.CreateTable(
+                name: "Albums",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AlbumName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ArtistName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    YearReleased = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AlbumGenre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AlbumDescription = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AlbumRating = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Albums", x => x.ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Albums");
+        }
+    }
+}
