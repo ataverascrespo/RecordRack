fa11510d62e7e7f056bbbdb5cda4b29b2f858bf4
Alex Taveras-Crespo <alexmanuelt@hotmail.com>
2022-12-21 20:10:16 -0500

    Creates auth. repository + interface

---
+++ Data/AuthRepository.cs
@@ -0,0 +1,11 @@
+/// <summary>
+/// Encapsulates logic required to access data sources
+/// No SQL queries required thanks to LINQ (language integrated query)
+/// </summary>
+namespace AlbumAPI.Data
+{
+    public class AuthRepository : IAuthRepository
+    {
+        
+    }
+}
---
+++ Data/IAuthRepository.cs
@@ -0,0 +1,10 @@
+/// <summary>
+/// Authentication repository method declarations
+/// </summary>
+namespace AlbumAPI.Data
+{
+    public class IAuthRepository
+    {
+        
+    }
+}
