b89473912a1a213ef4c533c5f060207550704587
Alex Taveras-Crespo <alexmanuelt@hotmail.com>
2022-12-22 10:27:36 -0500

    Adds security definitions

--- Program.cs
+++ Program.cs
@@ -9,6 +9,8 @@ global using AlbumAPI.Data;
 global using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using Swashbuckle.AspNetCore.Filters;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,7 +21,16 @@ builder.Services.AddDbContext<DataContext>(options =>
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(c => {
+    c.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme {
+        Description = """ Standard Authorization header using Bearer scheme. Example "bearer {token}" """,
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+    c.OperationFilter<SecurityRequirementsOperationFilter>();
+});
 
 //Implements NuGet AutoMapper service
 builder.Services.AddAutoMapper(typeof(Program).Assembly);
