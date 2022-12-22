52e8350a2ebca21c1334b5677cbceb9a2a1c90de
Alex Taveras-Crespo <alexmanuelt@hotmail.com>
2022-12-21 20:37:42 -0500

    Adds properties to UserRegisterDTO

--- DTOs/User/UserRegisterDTO.cs
+++ DTOs/User/UserRegisterDTO.cs
@@ -2,6 +2,7 @@ namespace AlbumAPI.DTOs
 {
     public class UserRegisterDTO
     {
-        
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
     }
 }
