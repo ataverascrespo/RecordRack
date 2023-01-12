c0b75e17fa8d67ad889371a14094ef8c462affb1
Alex Taveras-Crespo <alexmanuelt@hotmail.com>
2023-01-12 10:04:58 -0500

    Updates EF migrations to add photo DB columns

---
+++ Migrations/20230112143608_AlbumPhoto.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AlbumAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlbumPhoto : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "photoID",
+                table: "Albums",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "photoURL",
+                table: "Albums",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "publicID",
+                table: "Albums",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "photoID",
+                table: "Albums");
+
+            migrationBuilder.DropColumn(
+                name: "photoURL",
+                table: "Albums");
+
+            migrationBuilder.DropColumn(
+                name: "publicID",
+                table: "Albums");
+        }
+    }
+}
