c0ee8153134e5d52635841b69072e5e758acc3ed
Alex Taveras-Crespo <alexmanuelt@hotmail.com>
2023-04-28 17:05:30 -0400

    Documentation changes for Three.js files

--- wwwroot/scripts/geometry.js
+++ wwwroot/scripts/geometry.js
@@ -2,20 +2,25 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
-//Sizes
+//Configure album size
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight,
 };
-
+  
+//Create a scene and camera render
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 1000);
 
+//Retrieve canvas
 const canvas = document.querySelector('.webgl');
+
+//Create and configure renderer
 const renderer = new THREE.WebGLRenderer({canvas, alpha: true});
 renderer.setSize(sizes.width, sizes.height);
 document.body.appendChild(renderer.domElement);
 
+//Specify camera positions
 camera.position.z = 45 / 180 * Math.PI;
 camera.position.y = 1;
 camera.position.x = 2;
@@ -33,31 +38,38 @@ window.addEventListener('resize', () => {
     renderer.setSize(sizes.width, sizes.height);
 });
 
+
+//Load a GLTF model from the directory
 let loader = new GLTFLoader();
 loader.load("models/scene.gltf", function (gtlf) {
     var record = gtlf.scene.children[0];
+    //Create a GSAP timeline, set the scale from 0 to preferred size upon window load
     const timeline = gsap.timeline({ defaults: { duration: 1 } });
     timeline.fromTo(record.scale, { z: 0, x: 0, y: 0 }, { z: 1.5, x: 1.5, y: 1.5 });
     scene.add(gtlf.scene);
+    
+    //Configure light position
+    var directionaLight = new THREE.DirectionalLight(0xffffff, 0.5);
+    directionaLight.position.set(-20, 35, 0);
+    scene.add(directionaLight);
+
+    //Configure orbit controls
+    const controls = new OrbitControls(camera, canvas);
+    controls.enableDamping = true;
+    controls.enablePan = false;
+    controls.enableZoom = false;
+    controls.autoRotate = true;
+    controls.autoRotateSpeed = 7.5;
+  
+    //Every frame, re-animate the spinning record
+    function animate() {
+        controls.update();
+        console.log(record.z);
+        requestAnimationFrame(animate);
+        renderer.render(scene, camera);
+    }
+
+    animate();
 })
 
-var directionaLight = new THREE.DirectionalLight(0xffffff, 0.5);
-directionaLight.position.set(-20, 35, 0);
-scene.add(directionaLight);
-
-const controls = new OrbitControls(camera, canvas);
-controls.enableDamping = true;
-controls.enablePan = false;
-controls.enableZoom = false;
-controls.autoRotate = true;
-controls.autoRotateSpeed = 7.5;
-
-
-
-function animate() {
-    controls.update();
-	requestAnimationFrame(animate);
-	renderer.render(scene, camera);
-}
-animate();
 
