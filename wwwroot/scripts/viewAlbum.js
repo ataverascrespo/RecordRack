b58047d57df36d2957980c3ce3e46a8ead15aa04
Alex Taveras-Crespo <alexmanuelt@hotmail.com>
2023-04-28 16:23:07 -0400

    Add Three.js model for spinning album when viewing

--- wwwroot/scripts/viewAlbum.js
+++ wwwroot/scripts/viewAlbum.js
@@ -1,4 +1,8 @@
 import { getJWTToken } from './token.js';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+
 
 //Global variables
 var albumID, photoURL, publicID, content;
@@ -52,35 +56,107 @@ function getAlbum() {
     .then((response) => response.json())
     .then((data) => {
 
-        //For each of the form elements, set the value to the retrieved API value
-        //This creates input preview on the form for viewing
-        let imgPreview = document.getElementById("output");
-        imgPreview.setAttribute('src', data.data.photoURL); 
+      //For each of the form elements, set the value to the retrieved API value
+      //This creates input preview on the form for viewing
+      //Set the passed ID in the browsers local storage as albumID
+      
+      photoURL = data.data.photoURL;
+
+      //Set each of the input elements to read only
+      let albumName = document.getElementById("album");
+      albumName.setAttribute('value', data.data.albumName);
+      albumName.readOnly = true;
+
+      let artistName = document.getElementById("artist");
+      artistName.setAttribute('value', data.data.artistName);
+      artistName.readOnly = true;
+
+      let yearReleased = document.getElementById("year");
+      yearReleased.setAttribute('value', data.data.yearReleased);
+      yearReleased.readOnly = true;
+
+      let albumGenre = document.getElementById("genre");
+      albumGenre.setAttribute('value', data.data.albumGenre);
+      albumGenre.readOnly = true;
         
-        //Set each of the input elements to read only
-        let albumName = document.getElementById("album");
-        albumName.setAttribute('value', data.data.albumName);
-        albumName.readOnly = true;
+      let albumRating = document.getElementById("rating");
+      albumRating.setAttribute('value', data.data.albumRating);
+      albumRating.readOnly = true;
 
-        let artistName = document.getElementById("artist");
-        artistName.setAttribute('value', data.data.artistName);
-        artistName.readOnly = true;
-
-        let yearReleased = document.getElementById("year");
-        yearReleased.setAttribute('value', data.data.yearReleased);
-        yearReleased.readOnly = true;
-
-        let albumGenre = document.getElementById("genre");
-        albumGenre.setAttribute('value', data.data.albumGenre);
-        albumGenre.readOnly = true;
-        
-        let albumRating = document.getElementById("rating");
-        albumRating.setAttribute('value', data.data.albumRating);
-        albumRating.readOnly = true;
+      loadDisplay();
         
     })
     .catch((error) => {
         console.log(error);
     });
 }
+
+/*
+* loadDisplay - display spinning album
+*/
+function loadDisplay() {
+  //Configure album size
+  const sizes = {
+    width: window.innerWidth / 2,
+    height: window.innerHeight,
+  };
+
+  //Create a scene and camera render
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 1000);
+
+  //Retrieve canvas
+  const canvas = document.querySelector('.webgl');
+  //Create and configure renderer
+  const renderer = new THREE.WebGLRenderer({canvas, alpha: true});
+  renderer.setSize(sizes.width, sizes.height);
+  document.body.appendChild(renderer.domElement);
+
+  const geometry = new THREE.BoxGeometry(2.5, 2.5, 0.15);
+  const texture = new THREE.TextureLoader().load(photoURL);
+
+  //Specify cube materials
+  var cubeMaterial = [
+    new THREE.MeshBasicMaterial({
+      color: 0x4e3a36  //left
+    }),
+    new THREE.MeshBasicMaterial({
+      color: 0x4e3a36, //right
+    }),
+    new THREE.MeshBasicMaterial({
+      color: 0x4e3a36 // top
+    }),
+    new THREE.MeshBasicMaterial({
+      color: 0x4e3a36// bottom
+    }),
+    new THREE.MeshBasicMaterial({
+      map: texture, //front
+    }),
+    new THREE.MeshBasicMaterial({
+      map: texture //front
+    })
+  ];
+  //Create the cube
+  const cube = new THREE.Mesh(geometry, cubeMaterial);
+  scene.add(cube);
+
+  camera.position.z = 4;
+
+  const controls = new OrbitControls(camera, canvas);
+  controls.enableDamping = true;
+  controls.enablePan = false;
+  controls.enableZoom = false;
+  controls.autoRotate = true;
+  controls.autoRotateSpeed = 2;
+
+  //Every frame, re-animate the spinning album
+  function animate() {
+    controls.update();
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+  }
+  animate();
+}
+
+
 
