e465bc74d956c7cf8b0dd850cef0890ae9124ea5
Alex Taveras-Crespo <alexmanuelt@hotmail.com>
2023-04-30 19:18:51 -0400

    Spotify search bug - rearranged function calls

--- wwwroot/scripts/search.js
+++ wwwroot/scripts/search.js
@@ -33,7 +33,7 @@ function checkToken() {
     }
     else {
         searchButton.disabled = false;
-        searchButton.addEventListener("click", searchAlbum);
+        searchButton.addEventListener("click", validateSearch);
     }
   }
   else {
@@ -43,44 +43,9 @@ function checkToken() {
 }
 
 /*
- * validateToken - Function to retrieve and validate Spotify API credentials for search usage
+ * validateSearch- Function to validate whether the search is actually valid
  */
-function validateToken() {
-    
-    fetch('https://recordrack.netlify.app/netlify/functions/spotifyFunction', {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-        },
-    })
-    .then((response) => response.json())
-    .then((data) => {
-        CLIENT_SECRET = data.apiKey;
-        console.log(data);
-    });
-    
-    //Send POST request to Spotify API with the required credentials
-    var authParameters = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        body: 'grant_type=client_credentials&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET
-    }
-    fetch('https://accounts.spotify.com/api/token', authParameters)
-        .then(result => result.json())
-        //Set the access token in local storage
-        .then(data => localStorage.setItem("spotiapi", data.access_token))
-        //Exception handler
-    .catch((error) => {
-        console.log(error);
-    });
-}
-
-/*
- * validateToken - Function to search for an album using Spotify API
- */
-function searchAlbum(event) {
+function validateSearch(event) {
     //Prevent the default form submission behavior
     event.preventDefault();
 
@@ -106,64 +71,109 @@ function searchAlbum(event) {
             //Store parsed ID if there is a regex match
             albumID = match[1];
 
-        //Call token validation function
-        validateToken();
-        
-        //Send POST request to the API with the user credentials
-            fetch(`https://api.spotify.com/v1/albums/${albumID}`, {
-                method: "GET",
-                headers: {
-                    "Content-Type": "application/json",
-                    //Append the spotify token credentials in the authorization header
-                    "Authorization": `Bearer ${localStorage.getItem("spotiapi")}`,
-                },
-            })
-                .then((response) => response.json())
-                .then((data) => {
-                    //If the search is unsuccessful, display an error to end user
-                    if (data === null) {
-                        errorMessage.innerHTML = "That album does not exist.";
-                    }
-                    //If search is successful, the API returns a JSON body with data pertinent to our 
-                    else {
-                        errorMessage.innerHTML = "";
-                        
-                        //Set the album name into local storage
-                        localStorage.setItem("spotifyAlbumName", data.name)
-
-                        //Parse the album's release year out of it's full date
-                        let parsedYear = data.release_date.split("-")[0];
-                        //Set the release year into local storage
-                        localStorage.setItem("spotifyAlbumYear", parsedYear)
-
-                        //Set the album image to the first returned image
-                        localStorage.setItem("spotifyAlbumImg", data.images[0].url)
-
-                        var names;
-                        //If there is only one artist associated, set the name to that artist
-                        if (data.artists.length <= 1) {
-                            names = data.artists[0].name;
-                        }
-                        //If there is more than one, concatenate all the artists into one string
-                        else {
-                            var artists = data.artists.map(artist => artist.name);
-                            names = artists.join(", ");
-                        }
-                        //Set the album artists to the name string
-                        localStorage.setItem("spotifyAlbumArtists", names);
-                
-                        //Route to add album page
-                        window.location.href = "/spotifyaddalbum.html";
-                    }
-                })
-                .catch((error) => {
-                    console.log(error);
-                });
+            //Call token validation function
+            validateToken(albumID);
         }
         else {
             //Display null alert
             errorMessage.innerHTML = "That is not a valid Spotify URL.";
         }
     }
+}
+
+
+/*
+ * validateToken - Function to retrieve and validate Spotify API credentials for search usage
+ */
+function validateToken(albumID) {
+    
+    //Call lambda function to get client secret key for spotify api
+    fetch('https://recordrack.netlify.app/netlify/functions/spotifyFunction', {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        CLIENT_SECRET = data.apiKey;
+        console.log(data);
+    });
+    
+    //Send POST request to Spotify API with the required credentials
+    var authParameters = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: 'grant_type=client_credentials&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET
+    }
+    fetch('https://accounts.spotify.com/api/token', authParameters)
+        .then(result => result.json())
+        //Set the access token in local storage
+        .then(data => {
+            localStorage.setItem("spotiapi", data.access_token);
+            searchAlbum(albumID);
+        })
+        //Exception handler
+    .catch((error) => {
+        console.log(error);
+    });
+}
+
+/*
+ * searchAlbum - Function to search the album with proper credentials
+ */
+function searchAlbum(albumID) {
+    //Send POST request to the API with the user credentials
+    fetch(`https://api.spotify.com/v1/albums/${albumID}`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            //Append the spotify token credentials in the authorization header
+            "Authorization": `Bearer ${localStorage.getItem("spotiapi")}`,
+        },
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        //If the search is unsuccessful, display an error to end user
+        if (data === null) {
+            errorMessage.innerHTML = "That album does not exist.";
+        }
+        //If search is successful, the API returns a JSON body with data pertinent to our 
+        else {
+            errorMessage.innerHTML = "";
+            
+            //Set the album name into local storage
+            localStorage.setItem("spotifyAlbumName", data.name)
+
+            //Parse the album's release year out of it's full date
+            let parsedYear = data.release_date.split("-")[0];
+            //Set the release year into local storage
+            localStorage.setItem("spotifyAlbumYear", parsedYear)
+
+            //Set the album image to the first returned image
+            localStorage.setItem("spotifyAlbumImg", data.images[0].url)
+
+            var names;
+            //If there is only one artist associated, set the name to that artist
+            if (data.artists.length <= 1) {
+                names = data.artists[0].name;
+            }
+            //If there is more than one, concatenate all the artists into one string
+            else {
+                var artists = data.artists.map(artist => artist.name);
+                names = artists.join(", ");
+            }
+            //Set the album artists to the name string
+            localStorage.setItem("spotifyAlbumArtists", names);
+    
+            //Route to add album page
+            window.location.href = "/spotifyaddalbum.html";
+        }
+    })
+    .catch((error) => {
+        console.log(error);
+    });
 }
 
