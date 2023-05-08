47cbbad1b7cbf960b317a079b9a22d3d1a950838
Alex Taveras-Crespo <alexmanuelt@hotmail.com>
2023-05-08 11:27:36 -0400

    Append file extension on downloaded API image

--- wwwroot/scripts/addSpotifyAlbum.js
+++ wwwroot/scripts/addSpotifyAlbum.js
@@ -17,7 +17,7 @@ const imageURL = URL.createObjectURL(imageBlog)
 
 const link = document.createElement('a');
 link.href = imageURL;
-link.download = 'RecordRack' + localStorage.getItem("spotifyAlbumName") + 'Image';
+link.download = 'RecordRack' + localStorage.getItem("spotifyAlbumName") + 'Image.jpg';
 document.body.appendChild(link);
 link.click();
 document.body.removeChild(link);
