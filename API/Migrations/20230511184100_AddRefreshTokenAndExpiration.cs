f8033b13874a6be89af5f85bbc80f62973ecb1c1
Alex Taveras-Crespo <alexmanuelt@hotmail.com>
2023-05-11 16:37:03 -0400

    New EF Migration to persist data model changes

---
+++ API/Migrations/20230511184100_AddRefreshTokenAndExpiration.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AlbumAPI.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRefreshTokenAndExpiration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RefreshToken",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RefreshTokenExpiration",
+                table: "Users",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RefreshToken",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "RefreshTokenExpiration",
+                table: "Users");
+        }
+    }
+}
