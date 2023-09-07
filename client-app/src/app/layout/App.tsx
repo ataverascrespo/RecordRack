32d44d93bd0e3ab16b33f8d0f14224d34b4ac819
Alex Taveras-Crespo <alexmanuelt@hotmail.com>
2023-09-06 23:01:12 -0400

    Creates some custom components

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -4,6 +4,31 @@ import { Album } from "../models/album";
 import Navbar from "./Navbar";
 import { ThemeProvider } from "@/components/theme-provider"
 
+"use client"
+ 
+import { zodResolver } from "@hookform/resolvers/zod"
+import * as z from "zod"
+ 
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { SearchForm } from "@/components/search-form";
+
+ 
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+})
+
 
 //Spotify Client ID
 const CLIENT_ID = "6b6f9f90098c40469273168487c49eb7";
@@ -60,10 +85,25 @@ function App() {
   }
 
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
       <div className="App">
         <Navbar></Navbar>
-        <button className="text-3xl font-bold" onClick={getSearch}>Call API</button>
+
+        <div className="h-[50vh] flex flex-col justify-center gap-12 items-start container">
+          
+          <div className="flex flex-col gap-4 items-start">
+              <h1 className="text-6xl font-bold">Add a new record to your collection</h1>
+              <div className="flex flex-row gap-2 items-center">
+                <h2 className="font-semibold text-base text-neutral-300 dark:text-neutral-700">Powered by</h2>
+                <img className="bg-light dark:bg-dark" src="./src/assets/spotify.svg" draggable="false"></img>
+              </div>          
+          </div>
+          
+          <SearchForm></SearchForm>
+        </div>
+
+
+        {/* <button className="text-3xl font-bold" onClick={getSearch}>Call API</button>
           <ul>
             {albums.map(album => (
               <li key={album.id}>
@@ -72,7 +112,7 @@ function App() {
                 ))}
               </li>
             ))}
-          </ul>
+          </ul> */}
       </div>
     </ThemeProvider>
   )
--- client-app/src/app/layout/Navbar.tsx
+++ client-app/src/app/layout/Navbar.tsx
@@ -1,19 +1,23 @@
 import React from 'react'
 import { Button } from "@/components/ui/button"
+import { ModeToggle } from '@/components/mode-toggle'
 
 
 export default function Navbar() {
     return (
-    <nav className="shadow-sm">    
+    <nav className="border-b-2 border-neutral-100 dark:border-neutral-900">    
         <div className="container py-6 flex flex-row items-center justify-between">
-            <div>
-              <h1 className="font-semibold text-2xl">Record Rack</h1>
+            <div className="flex flex-row gap-6 items-center">
+                    <h1 className="font-semibold text-2xl">Record Rack</h1>
+                    <ModeToggle></ModeToggle>
+
           </div>
           <div>
             <Button variant="ghost" size="lg"><p className="text-lg">Home</p></Button>
             <Button variant="ghost" size="lg"><p className="text-lg">Your Rack</p></Button>
             <Button variant="ghost" size="lg"><p className="text-lg">About</p></Button>
-          </div>
+                </div>
+                
         </div>
     </nav>
   )
