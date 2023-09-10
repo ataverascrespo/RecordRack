aa10bcb2968d39a534cff6b03fdead746fd505ce
Alex Taveras-Crespo <alexmanuelt@hotmail.com>
2023-09-10 17:02:43 -0400

    Adds empty rack list indicator

--- client-app/src/features/rack/RackPage.tsx
+++ client-app/src/features/rack/RackPage.tsx
@@ -1,30 +1,42 @@
-import { ThemeProvider } from "@/components/theme-provider"
 import { useStore } from "@/app/stores/store";
 import { observer } from "mobx-react-lite";
 import RackList from "@/features/rack/RackList";
+import { Button } from "@/components/ui/button";
+import { NavLink } from "react-router-dom";
 
 function SearchPage() {
     // Access the global Mobx stores
     const { searchStore } = useStore();
 
     return (
-        <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-            <div className="container">
-                <div className="h-full mt-[9.75rem] mb-24 flex flex-col justify-start gap-12 items-start ">
-                    {/* Your own record rack */}
-                    <div className="flex flex-col gap-2 items-start">
-                        <h2 className="font-semibold text-sm sm:text-base md:text-lg xl:text-xl text-neutral-400 dark:text-neutral-600">Hey username,</h2>
-                        <h1 className="text-2xl md:text-4xl lg:text-6xl font-bold text-neutral-800 dark:text-neutral-50">Welcome to your record rack</h1>
-                    </div>
+        <div className="container">
+            <div className="h-full mt-[9.75rem] mb-24 flex flex-col justify-start gap-12 items-start ">
+                {/* Your own record rack */}
+                <div className="flex flex-col gap-2 items-start">
+                    <h2 className="font-semibold text-sm sm:text-base md:text-lg xl:text-xl text-neutral-400 dark:text-neutral-600">Hey username,</h2>
+                    <h1 className="text-2xl md:text-4xl lg:text-6xl font-bold text-neutral-800 dark:text-neutral-50">Welcome to your record rack</h1>
+                </div>
 
-                    {/* <div className="flex flex-col gap-2 items-start">
+                {/* <div className="flex flex-col gap-2 items-start">
                         <h2 className="font-semibold text-sm sm:text-base md:text-lg xl:text-xl text-neutral-400 dark:text-neutral-600">Hey username,</h2>
                         <h1 className="text-2xl md:text-4xl lg:text-6xl font-bold text-neutral-800 dark:text-neutral-50">This is OtherUsername's record rack</h1>
                     </div> */}
-                    <RackList results={searchStore.searchAlbums}></RackList>
-                </div>
+
+                {searchStore.searchAlbums.length != 0
+                    ? <RackList results={searchStore.searchAlbums}></RackList>
+                    :
+                    <div className="lg:h-[50vh] z-0 mt-48 lg:mt-0 flex flex-col justify-end lg:flex-row lg:justify-start items-center">
+                        <div className="flex flex-col items-center lg:items-start gap-6">
+                            <h2 className="max-w-2xl mt-6 text-base lg:text-xl text-neutral-600dark:text-neutral-300">Looks like your rack is empty! Let's search for some records...</h2>
+                            <NavLink to={"/search"}>
+                                <Button size="lg"><p className="text-base">Search</p></Button>
+                            </NavLink>
+                        </div>
+                        <img className="w-[15%] mt-12 lg:mt-0" src="./src/assets/empty.svg" alt="hero" draggable="false" />
+                    </div>
+                }
             </div>
-        </ThemeProvider>
+        </div>
     )
 }
 
