8be63f8742b9dc2a44552895b11cd56b7e25d62a
Alex Taveras-Crespo <alexmanuelt@hotmail.com>
2023-09-11 11:56:03 -0400

    Added reset and profile routes

--- client-app/src/app/router/Routes.tsx
+++ client-app/src/app/router/Routes.tsx
@@ -8,6 +8,8 @@ import RackView from "@/features/rack/RackView";
 import RegisterPage from "@/features/auth/RegisterPage";
 import LoginPage from "@/features/auth/LoginPage";
 import NotFound from "../layout/NotFound";
+import ProfilePage from "@/features/profile/ProfilePage";
+import PasswordForgotPage from "@/features/auth/Password/PasswordForgotPage";
 
 
 // Define the routes of the MPA
@@ -22,8 +24,10 @@ export const routes: RouteObject[] = [
             { path: 'record/:id', element: <RackView /> },
             { path: 'search', element: <SearchPage /> },
             { path: 'about', element: <AboutPage /> },
+            { path: 'profile', element: <ProfilePage /> },
             { path: 'register', element: <RegisterPage /> },
             { path: 'login', element: <LoginPage /> },
+            { path: 'forgotPassword', element: <PasswordForgotPage /> },
 
             { path: '*', element: <NotFound /> },
 
