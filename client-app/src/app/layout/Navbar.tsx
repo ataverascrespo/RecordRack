2cd69316bc293d8e27b0fccc81d480db2556c059
Alex Taveras-Crespo <alexmanuelt@hotmail.com>
2023-09-13 13:48:29 -0400

    Adds dynamic rendering to navbar based on login

--- client-app/src/app/layout/Navbar.tsx
+++ client-app/src/app/layout/Navbar.tsx
@@ -3,87 +3,87 @@ import { ModeToggle } from '@/components/mode-toggle'
 import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuList, NavigationMenuTrigger } from "@/components/ui/navigation-menu"
 import { NavLink } from "react-router-dom"
 import { Avatar, AvatarFallback, AvatarImage, } from "@/components/ui/avatar"
+import { useStore } from "../stores/store"
+import { observer } from "mobx-react-lite"
 
-export default function Navbar() {
-
-  const isUserLoggedIn = true;
+function Navbar() {
+  //Initialize user store with user object and loggedIn check
+  const { userStore: { user, isLoggedIn } } = useStore();
 
   return (
     <nav className="sticky top-0 z-50 -mt-24 border-b-2 shadow-md bg-background border-neutral-100 dark:border-neutral-950 text-neutral-800 dark:text-neutral-50">
 
-      {isUserLoggedIn
-      /*
-        WHEN USER IS SIGNED IN
-      */
-      ? <div className="container py-6 flex flex-row items-center justify-between">
-        <div className="flex flex-row gap-2 lg:gap-6 items-center">
-          <NavLink to={"/"}>
-            <h1 className="font-semibold text-base lg:text-xl xl:text-2xl">Record Rack</h1>
-          </NavLink>
-          <ModeToggle></ModeToggle>
-        </div>
-
-        <div className='hidden md:flex flex-row'>
-          <NavLink to={"/racklist"}>
-            <Button variant="ghost" size="lg"><p className="text-sm lg:text-base xl:text-lg">your rack</p></Button>
-          </NavLink>
-          <NavLink to={"/search"}>
-            <Button variant="ghost" size="lg"><p className="text-sm lg:text-base xl:text-lg">search</p></Button>
-          </NavLink>
-          <NavLink to={"/"}>
-            <Button variant="ghost" size="lg"><p className="text-sm lg:text-base xl:text-lg">connect</p></Button>
-          </NavLink>
-          <NavLink to={"/about"}>
-            <Button variant="ghost" size="lg"><p className="text-sm lg:text-base xl:text-lg">about</p></Button>
-          </NavLink>
-          <div className="ml-8">
+      {/* Check userstore logged in method to determine if user is logged in */}
+      {isLoggedIn
+        ? <div className="container py-6 flex flex-row items-center justify-between">
+          <div className="flex flex-row gap-2 lg:gap-6 items-center">
             <NavLink to={"/"}>
-              <Avatar>
-                <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-                <AvatarFallback>CN</AvatarFallback>
-              </Avatar>
+              <h1 className="font-semibold text-base lg:text-xl xl:text-2xl">Record Rack</h1>
             </NavLink>
+            <ModeToggle></ModeToggle>
+          </div>
+
+          <div className='hidden md:flex flex-row'>
+            <NavLink to={`/racklist/${user?.userName}`}>
+              <Button variant="ghost" size="lg"><p className="text-sm lg:text-base xl:text-lg">your rack</p></Button>
+            </NavLink>
+            <NavLink to={"/search"}>
+              <Button variant="ghost" size="lg"><p className="text-sm lg:text-base xl:text-lg">search</p></Button>
+            </NavLink>
+            <NavLink to={"/"}>
+              <Button variant="ghost" size="lg"><p className="text-sm lg:text-base xl:text-lg">connect</p></Button>
+            </NavLink>
+            <NavLink to={"/about"}>
+              <Button variant="ghost" size="lg"><p className="text-sm lg:text-base xl:text-lg">about</p></Button>
+            </NavLink>
+            <div className="ml-8">
+              <NavLink to={"/profile"}>
+                <Avatar>
+                  <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                  <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+              </NavLink>
+            </div>
+          </div>
+
+          <div className="block md:hidden">
+            <NavigationMenu className="relative -left-32">
+              <NavigationMenuList>
+                <NavigationMenuItem>
+                  <NavigationMenuTrigger className="relative left-32"></NavigationMenuTrigger>
+                  <NavigationMenuContent >
+                    <ul className="grid gap-3 p-4 w-[170px] md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+                      <li className="row-span-3">
+                        <NavLink to={"/racklist"}>
+                          <Button variant="ghost" size="lg"><p className="text-base">your rack</p></Button>
+                        </NavLink>
+                        <NavLink to={"/search"}>
+                          <Button variant="ghost" size="lg"><p className="text-base">search</p></Button>
+                        </NavLink>
+                        <NavLink to={"/about"}>
+                          <Button variant="ghost" size="lg"><p className="text-base">about</p></Button>
+                        </NavLink>
+                        <div className="ml-8 my-2">
+                          <NavLink to={"/"}>
+                            <Avatar>
+                              <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                              <AvatarFallback>CN</AvatarFallback>
+                            </Avatar>
+                          </NavLink>
+                        </div>
+                      </li>
+                    </ul>
+                  </NavigationMenuContent>
+                </NavigationMenuItem>
+              </NavigationMenuList>
+            </NavigationMenu>
           </div>
         </div>
 
-        <div className="block md:hidden">
-          <NavigationMenu className="relative -left-32">
-            <NavigationMenuList>
-              <NavigationMenuItem>
-                <NavigationMenuTrigger className="relative left-32"></NavigationMenuTrigger>
-                <NavigationMenuContent >
-                  <ul className="grid gap-3 p-4 w-[170px] md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-                    <li className="row-span-3">
-                      <NavLink to={"/racklist"}>
-                        <Button variant="ghost" size="lg"><p className="text-base">your rack</p></Button>
-                      </NavLink>
-                      <NavLink to={"/search"}>
-                        <Button variant="ghost" size="lg"><p className="text-base">search</p></Button>
-                      </NavLink>
-                      <NavLink to={"/about"}>
-                        <Button variant="ghost" size="lg"><p className="text-base">about</p></Button>
-                      </NavLink>
-                      <div className="ml-8 my-2">
-                        <NavLink to={"/"}>
-                          <Avatar>
-                            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-                            <AvatarFallback>CN</AvatarFallback>
-                          </Avatar>
-                        </NavLink>
-                      </div>
-                    </li>
-                  </ul>
-                </NavigationMenuContent>
-              </NavigationMenuItem>
-            </NavigationMenuList>
-          </NavigationMenu>
-        </div>
-      </div>
-
-      /*
-        WHEN USER IS NOT SIGNED IN
-      */
-      : <div className="container py-6 flex flex-row items-center justify-between">
+        /*
+          WHEN USER IS NOT SIGNED IN
+        */
+        : <div className="container py-6 flex flex-row items-center justify-between">
           <div className="flex flex-row gap-2 lg:gap-6 items-center">
             <NavLink to={"/"}>
               <h1 className="font-semibold text-base lg:text-xl xl:text-2xl">Record Rack</h1>
@@ -132,3 +132,5 @@ export default function Navbar() {
     </nav>
   )
 }
+
+export default observer(Navbar)
