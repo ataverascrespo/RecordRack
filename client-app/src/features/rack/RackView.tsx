ec4fa8ad0e61bcf54f6505ae547b430c80557269
Alex Taveras-Crespo <alexmanuelt@hotmail.com>
2023-09-13 23:11:57 -0400

    Fixes invalid DOM SVG camel-case error

--- client-app/src/features/rack/RackView.tsx
+++ client-app/src/features/rack/RackView.tsx
@@ -28,7 +28,7 @@ function RackView() {
         */
         if (!isMounted.current) {
             if (params.id) {
-                recordStore.getRecordByID((parseInt(params.id, 10)));
+                //recordStore.getRecordByID((parseInt(params.id, 10)));
             }
         }
         //Set this to true after the initial render
@@ -42,7 +42,7 @@ function RackView() {
                 {/* Image */}
                 <div className="flex flex-col mt-32 w-full sm:w-3/4 md:w-2/3 gap-6 items-start justify-between lg:justify-center sm:self-start md:self-center">
                     <Button variant="link" className="pl-0">
-                        <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.84182 3.13514C9.04327 3.32401 9.05348 3.64042 8.86462 3.84188L5.43521 7.49991L8.86462 11.1579C9.05348 11.3594 9.04327 11.6758 8.84182 11.8647C8.64036 12.0535 8.32394 12.0433 8.13508 11.8419L4.38508 7.84188C4.20477 7.64955 4.20477 7.35027 4.38508 7.15794L8.13508 3.15794C8.32394 2.95648 8.64036 2.94628 8.84182 3.13514Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path>
+                        <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.84182 3.13514C9.04327 3.32401 9.05348 3.64042 8.86462 3.84188L5.43521 7.49991L8.86462 11.1579C9.05348 11.3594 9.04327 11.6758 8.84182 11.8647C8.64036 12.0535 8.32394 12.0433 8.13508 11.8419L4.38508 7.84188C4.20477 7.64955 4.20477 7.35027 4.38508 7.15794L8.13508 3.15794C8.32394 2.95648 8.64036 2.94628 8.84182 3.13514Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path>
                         </svg>
                         <p className="text-xs">Back to {user?.userName}'s record rack</p>
                     </Button>
