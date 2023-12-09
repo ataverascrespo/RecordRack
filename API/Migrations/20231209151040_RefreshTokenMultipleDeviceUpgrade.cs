8c0605827c808c89ac9f6bab2968e7afb46fef64
Alex Taveras-Crespo <alexmanuelt@hotmail.com>
2023-12-09 10:33:45 -0500

    #7 - Completes new EF migration for token changes

---
+++ API/Migrations/20231209151040_RefreshTokenMultipleDeviceUpgrade.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using AlbumAPI.Models;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AlbumAPI.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class RefreshTokenMultipleDeviceUpgrade : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RefreshToken",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "RefreshTokenExpiration",
+                table: "Users");
+
+            migrationBuilder.AddColumn<List<RefreshToken>>(
+                name: "RefreshTokens",
+                table: "Users",
+                type: "jsonb",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RefreshTokens",
+                table: "Users");
+
+            migrationBuilder.AddColumn<string>(
+                name: "RefreshToken",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RefreshTokenExpiration",
+                table: "Users",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
