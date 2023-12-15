beeee1666d27a53716d34ffd28007daf9cec780c
Alex Taveras-Crespo <alexmanuelt@hotmail.com>
2023-12-15 00:32:50 -0500

    Completes XUnit UserService test cases (24/24 passing)

--- APITests/UnitTests/UserServiceTests.cs
+++ APITests/UnitTests/UserServiceTests.cs
@@ -169,8 +169,13 @@ public class UserServiceTests
         // Mock the HTTPContextAccessor properties
         var userIdClaim = new Claim(ClaimTypes.NameIdentifier, "123");
         var user = new ClaimsPrincipal(new ClaimsIdentity(new[] { userIdClaim }, "mock"));
+        // Stub the private GetUserID() method
+        httpContextAccessor.HttpContext.Returns(new DefaultHttpContext
+        {
+            User = user
+        });
 
-        // Create non-empty list of dummy users
+        // Create dummy user
         var dummyUser = new User { ID = 1, UserName = "TestUser", Followers = { }, Followings = { } };
         // Create search query 
         var searchQuery = "TestUser";
@@ -188,10 +193,7 @@ public class UserServiceTests
                 FollowingCount = user.Followings.Count,
             };
         });
-        httpContextAccessor.HttpContext.Returns(new DefaultHttpContext
-        {
-            User = user
-        });
+        
 
         // Act
         var result = await userService.GetUserByName(searchQuery);
@@ -213,6 +215,11 @@ public class UserServiceTests
         // Mock the HTTPContextAccessor properties
         var userIdClaim = new Claim(ClaimTypes.NameIdentifier, "123");
         var user = new ClaimsPrincipal(new ClaimsIdentity(new[] { userIdClaim }, "mock"));
+        // Stub the private GetUserID() method
+        httpContextAccessor.HttpContext.Returns(new DefaultHttpContext
+        {
+            User = user
+        });
 
         // Create search query 
         var searchQuery = "TestUser";
@@ -229,10 +236,6 @@ public class UserServiceTests
                 FollowersCount = user.Followers.Count,
                 FollowingCount = user.Followings.Count,
             };
-        });
-        httpContextAccessor.HttpContext.Returns(new DefaultHttpContext
-        {
-            User = user
         });
 
         // Act
@@ -256,16 +259,17 @@ public class UserServiceTests
         // Mock the HTTPContextAccessor properties
         var userIdClaim = new Claim(ClaimTypes.NameIdentifier, "1");
         var user = new ClaimsPrincipal(new ClaimsIdentity(new[] { userIdClaim }, "mock"));
-
-        // Create non-empty list of dummy users
-        var dummyUser = new User { ID = 1, UserName = "CurrentUser"};
-    
-        // Stub the repository method(s) return value(s)
-        repository.GetCurrentUser(1).Returns(Task.FromResult(dummyUser));
+        // Stub the private GetUserID() method
         httpContextAccessor.HttpContext.Returns(new DefaultHttpContext
         {
             User = user
         });
+
+        // Create dummy user
+        var dummyUser = new User { ID = 1, UserName = "CurrentUser"};
+    
+        // Stub the repository method(s) return value(s)
+        repository.GetCurrentUser(1).Returns(Task.FromResult(dummyUser));
 
         // Act
         var result = await userService.GetCurrentUser();
@@ -289,17 +293,18 @@ public class UserServiceTests
         // Mock the HTTPContextAccessor properties
         var userIdClaim = new Claim(ClaimTypes.NameIdentifier, "1");
         var user = new ClaimsPrincipal(new ClaimsIdentity(new[] { userIdClaim }, "mock"));
-
-        // Create non-empty list of dummy users
-        var dummyUser = new User { ID = 1, UserName = "CurrentUser"};
-    
-        // Stub the repository method(s) return value(s)
-        repository.GetCurrentUser(2).Returns(Task.FromResult(dummyUser));
+        // Stub the private GetUserID() method
         httpContextAccessor.HttpContext.Returns(new DefaultHttpContext
         {
             User = user
         });
 
+        // Create dummy user
+        var dummyUser = new User { ID = 1, UserName = "CurrentUser"};
+    
+        // Stub the repository method(s) return value(s)
+        repository.GetCurrentUser(2).Returns(Task.FromResult(dummyUser));
+       
         // Act
         var result = await userService.GetCurrentUser();
 
@@ -308,4 +313,312 @@ public class UserServiceTests
         Assert.False(result.Success);
     }
 
+    [Fact]
+    public async void FollowUser_UserNotFollowed_ReturnsSuccessfulFollow()
+    {
+        // Arrange
+        // Create repository subsitute and new service instance
+        var repository = Substitute.For<IUserRepository>();
+        var mapper = Substitute.For<IMapper>();
+        var httpContextAccessor = Substitute.For<IHttpContextAccessor>();
+        var userService = new UserService(mapper, repository, httpContextAccessor);
+
+        // Mock the HTTPContextAccessor properties
+        var userIdClaim = new Claim(ClaimTypes.NameIdentifier, "1");
+        var user = new ClaimsPrincipal(new ClaimsIdentity(new[] { userIdClaim }, "mock"));
+        // Stub the private GetUserID() method
+        httpContextAccessor.HttpContext.Returns(new DefaultHttpContext
+        {
+            User = user
+        });
+
+        // Create dummy data to be used for stubs
+        var dummyFollower = new User { ID = 1, UserName = "Follower"};
+        var dummyTarget = new User { ID = 2, UserName = "Target"};
+        var dummyFollowing = new UserFollowing { FollowerID = 1, TargetID = 2 };
+    
+        // Stub the repository method(s) return value(s)
+        repository.FollowUserGetFollower(1).Returns(Task.FromResult(dummyFollower));
+        repository.FollowUserGetTarget(2).Returns(Task.FromResult(dummyTarget));
+        repository.FindFollowing(dummyFollower.ID, dummyTarget.ID).Returns(Task.FromResult<UserFollowing?>(null));
+        repository.AddFollowing(dummyFollowing).Returns(Task.CompletedTask);
+        repository.SaveChanges().Returns(Task.CompletedTask);
+        repository.GetUserFollowersCount(dummyFollower.ID).Returns(Task.FromResult(0));
+        repository.GetUserFollowingCount(dummyFollower.ID).Returns(Task.FromResult(1));
+
+        // Act
+        var result = await userService.FollowUser(2);
+
+        // Assert
+        Assert.NotNull(result.Data);
+        Assert.True(result.Success);
+        Assert.Equal("Followed user.", result.ReturnMessage);
+    }
+
+    [Fact]
+    public async void FollowUser_UserWasFollowed_ReturnsSuccessfulUnfollow()
+    {
+        // Arrange
+        // Create repository subsitute and new service instance
+        var repository = Substitute.For<IUserRepository>();
+        var mapper = Substitute.For<IMapper>();
+        var httpContextAccessor = Substitute.For<IHttpContextAccessor>();
+        var userService = new UserService(mapper, repository, httpContextAccessor);
+
+        // Mock the HTTPContextAccessor properties
+        var userIdClaim = new Claim(ClaimTypes.NameIdentifier, "1");
+        var user = new ClaimsPrincipal(new ClaimsIdentity(new[] { userIdClaim }, "mock"));
+        // Stub the private GetUserID() method
+        httpContextAccessor.HttpContext.Returns(new DefaultHttpContext
+        {
+            User = user
+        });
+
+        // Create dummy data to be used for stubs
+        var dummyFollower = new User { ID = 1, UserName = "Follower"};
+        var dummyTarget = new User { ID = 2, UserName = "Target"};
+        var dummyFollowing = new UserFollowing { FollowerID = 1, TargetID = 2 };
+    
+        // Stub the repository method(s) return value(s)
+        repository.FollowUserGetFollower(1).Returns(Task.FromResult(dummyFollower));
+        repository.FollowUserGetTarget(2).Returns(Task.FromResult(dummyTarget));
+        repository.FindFollowing(dummyFollower.ID, dummyTarget.ID).Returns(Task.FromResult(dummyFollowing));
+        repository.RemoveFollowing(dummyFollowing).Returns(Task.CompletedTask);
+        repository.SaveChanges().Returns(Task.CompletedTask);
+        repository.GetUserFollowersCount(dummyFollower.ID).Returns(Task.FromResult(0));
+        repository.GetUserFollowingCount(dummyFollower.ID).Returns(Task.FromResult(0));
+
+        // Act
+        var result = await userService.FollowUser(2);
+
+        // Assert
+        Assert.NotNull(result.Data);
+        Assert.True(result.Success);
+        Assert.Equal("Unfollowed user.", result.ReturnMessage);
+    }
+
+    [Fact]
+    public async void GetFollowers_UserFound_ReturnsListOfFollowers()
+    {
+        // Arrange
+        // Create repository subsitute and new service instance
+        var repository = Substitute.For<IUserRepository>();
+        var mapper = Substitute.For<IMapper>();
+        var httpContextAccessor = Substitute.For<IHttpContextAccessor>();
+        var userService = new UserService(mapper, repository, httpContextAccessor);
+
+        // Mock the HTTPContextAccessor properties
+        // Current USER ID will be 4
+        var userIdClaim = new Claim(ClaimTypes.NameIdentifier, "4");
+        var user = new ClaimsPrincipal(new ClaimsIdentity(new[] { userIdClaim }, "mock"));
+        // Stub the private GetUserID() method
+        httpContextAccessor.HttpContext.Returns(new DefaultHttpContext
+        {
+            User = user
+        });
+
+        // Create dummy target to search followers
+        var dummyTarget = new User { ID = 1, UserName = "TestUserOne", Followers = { }, Followings = { } };
+        // Create non-empty list of dummy followers
+        var dummyFollowers = new List<UserDTO>
+        {
+            new UserDTO { ID = 2, UserName = "TestUserTwo"},
+            new UserDTO { ID = 3, UserName = "TestUserTwo"},
+        };
+
+        // Stub the repository method(s) return value(s)
+        repository.GetUserByIDWithFollowers(dummyTarget.ID).Returns(Task.FromResult(dummyTarget));
+        // Pass dummy target ID = 1 and current user ID = 4
+        repository.GetFollowers(dummyTarget.ID, 4).Returns(Task.FromResult(dummyFollowers));
+
+        // Act
+        var result = await userService.GetFollowers(dummyTarget.ID);
+
+        // Assert
+        Assert.True(result.Data!.Count == 2);
+        Assert.True(result.Success);
+    }
+
+    [Fact]
+    public async void GetFollowers_UserFound_ReturnsEmptyListOfFollowers()
+    {
+        // Arrange
+        // Create repository subsitute and new service instance
+        var repository = Substitute.For<IUserRepository>();
+        var mapper = Substitute.For<IMapper>();
+        var httpContextAccessor = Substitute.For<IHttpContextAccessor>();
+        var userService = new UserService(mapper, repository, httpContextAccessor);
+
+        // Mock the HTTPContextAccessor properties
+        // Current USER ID will be 4
+        var userIdClaim = new Claim(ClaimTypes.NameIdentifier, "4");
+        var user = new ClaimsPrincipal(new ClaimsIdentity(new[] { userIdClaim }, "mock"));
+        // Stub the private GetUserID() method
+        httpContextAccessor.HttpContext.Returns(new DefaultHttpContext
+        {
+            User = user
+        });
+
+        // Create dummy target to search followers
+        var dummyTarget = new User { ID = 1, UserName = "TestUserOne", Followers = { }, Followings = { } };
+
+        // Stub the repository method(s) return value(s)
+        repository.GetUserByIDWithFollowers(dummyTarget.ID).Returns(Task.FromResult(dummyTarget));
+        // Pass dummy target ID = 1 and current user ID = 4
+        repository.GetFollowers(dummyTarget.ID, 4).Returns(Task.FromResult<List<UserDTO>?>(null));
+
+        // Act
+        var result = await userService.GetFollowers(dummyTarget.ID);
+
+        // Assert
+        Assert.Null(result.Data);
+        Assert.True(result.Success);
+    }
+
+    [Fact]
+    public async void GetFollowers_UserNotFound_ReturnsNull_ReturnsFalseSuccess()
+    {
+        // Arrange
+        // Create repository subsitute and new service instance
+        var repository = Substitute.For<IUserRepository>();
+        var mapper = Substitute.For<IMapper>();
+        var httpContextAccessor = Substitute.For<IHttpContextAccessor>();
+        var userService = new UserService(mapper, repository, httpContextAccessor);
+
+        // Mock the HTTPContextAccessor properties
+        // Current USER ID will be 4
+        var userIdClaim = new Claim(ClaimTypes.NameIdentifier, "4");
+        var user = new ClaimsPrincipal(new ClaimsIdentity(new[] { userIdClaim }, "mock"));
+        // Stub the private GetUserID() method
+        httpContextAccessor.HttpContext.Returns(new DefaultHttpContext
+        {
+            User = user
+        });
+
+        // Create dummy target to search followers
+        var dummyTarget = new User { ID = 1, UserName = "TestUserOne", Followers = { }, Followings = { } };
+
+        // Stub the repository method(s) return value(s)
+        repository.GetUserByIDWithFollowers(dummyTarget.ID).Returns(Task.FromResult<User?>(null));
+
+        // Act
+        var result = await userService.GetFollowers(dummyTarget.ID);
+
+        // Assert
+        Assert.Null(result.Data);
+        Assert.False(result.Success);
+    }
+
+    [Fact]
+    public async void GetFollowing_UserFound_ReturnsListOfFollowing()
+    {
+        // Arrange
+        // Create repository subsitute and new service instance
+        var repository = Substitute.For<IUserRepository>();
+        var mapper = Substitute.For<IMapper>();
+        var httpContextAccessor = Substitute.For<IHttpContextAccessor>();
+        var userService = new UserService(mapper, repository, httpContextAccessor);
+
+        // Mock the HTTPContextAccessor properties
+        // Current USER ID will be 4
+        var userIdClaim = new Claim(ClaimTypes.NameIdentifier, "4");
+        var user = new ClaimsPrincipal(new ClaimsIdentity(new[] { userIdClaim }, "mock"));
+        // Stub the private GetUserID() method
+        httpContextAccessor.HttpContext.Returns(new DefaultHttpContext
+        {
+            User = user
+        });
+
+        // Create dummy target to search followers
+        var dummyFollower = new User { ID = 1, UserName = "TestUserOne", Followers = { }, Followings = { } };
+        // Create non-empty list of dummy followers
+        var dummyFollowings = new List<UserDTO>
+        {
+            new UserDTO { ID = 2, UserName = "TestUserTwo"},
+            new UserDTO { ID = 3, UserName = "TestUserTwo"},
+        };
+
+        // Stub the repository method(s) return value(s)
+        repository.GetUserByIDWithFollowers(dummyFollower.ID).Returns(Task.FromResult(dummyFollower));
+        // Pass dummy target ID = 1 and current user ID = 4
+        repository.GetFollowings(dummyFollower.ID, 4).Returns(Task.FromResult(dummyFollowings));
+
+        // Act
+        var result = await userService.GetFollowing(dummyFollower.ID);
+
+        // Assert
+        Assert.True(result.Data!.Count == 2);
+        Assert.True(result.Success);
+    }
+
+    [Fact]
+    public async void GetFollowing_UserFound_ReturnsEmptyListOfFollowing()
+    {
+        // Arrange
+        // Create repository subsitute and new service instance
+        var repository = Substitute.For<IUserRepository>();
+        var mapper = Substitute.For<IMapper>();
+        var httpContextAccessor = Substitute.For<IHttpContextAccessor>();
+        var userService = new UserService(mapper, repository, httpContextAccessor);
+
+        // Mock the HTTPContextAccessor properties
+        // Current USER ID will be 4
+        var userIdClaim = new Claim(ClaimTypes.NameIdentifier, "4");
+        var user = new ClaimsPrincipal(new ClaimsIdentity(new[] { userIdClaim }, "mock"));
+        // Stub the private GetUserID() method
+        httpContextAccessor.HttpContext.Returns(new DefaultHttpContext
+        {
+            User = user
+        });
+
+        // Create dummy target to search followers
+        var dummyFollower = new User { ID = 1, UserName = "TestUserOne", Followers = { }, Followings = { } };
+
+        // Stub the repository method(s) return value(s)
+        repository.GetUserByIDWithFollowers(dummyFollower.ID).Returns(Task.FromResult(dummyFollower));
+        // Pass dummy target ID = 1 and current user ID = 4
+        repository.GetFollowings(dummyFollower.ID, 4).Returns(Task.FromResult<List<UserDTO>?>(null));
+
+        // Act
+        var result = await userService.GetFollowing(dummyFollower.ID);
+
+        // Assert
+        Assert.Null(result.Data);
+        Assert.True(result.Success);
+    }
+
+    [Fact]
+    public async void GetFollowing_UserNotFound_ReturnsNull_ReturnsFalseSuccess()
+    {
+        // Arrange
+        // Create repository subsitute and new service instance
+        var repository = Substitute.For<IUserRepository>();
+        var mapper = Substitute.For<IMapper>();
+        var httpContextAccessor = Substitute.For<IHttpContextAccessor>();
+        var userService = new UserService(mapper, repository, httpContextAccessor);
+
+        // Mock the HTTPContextAccessor properties
+        // Current USER ID will be 4
+        var userIdClaim = new Claim(ClaimTypes.NameIdentifier, "4");
+        var user = new ClaimsPrincipal(new ClaimsIdentity(new[] { userIdClaim }, "mock"));
+        // Stub the private GetUserID() method
+        httpContextAccessor.HttpContext.Returns(new DefaultHttpContext
+        {
+            User = user
+        });
+
+        // Create dummy target to search followers
+        var dummyFollower = new User { ID = 1, UserName = "TestUserOne", Followers = { }, Followings = { } };
+
+        // Stub the repository method(s) return value(s)
+        repository.GetUserByIDWithFollowers(dummyFollower.ID).Returns(Task.FromResult<User?>(null));
+
+        // Act
+        var result = await userService.GetFollowing(dummyFollower.ID);
+
+        // Assert
+        Assert.Null(result.Data);
+        Assert.False(result.Success);
+    }
+    
 }
