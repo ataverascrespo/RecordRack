2eebb8444be7826bb21209f41c72d0394e3911b0
Alex Taveras-Crespo <alexmanuelt@hotmail.com>
2024-05-22 09:56:10 -0400

    Modify start script spacing

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -13,7 +13,4 @@ SpotifySettings_ClientID=$(aws ssm get-parameter --name "SpotifySettings_ClientI
 SpotifySettings_ClientSecret=$(aws ssm get-parameter --name "SpotifySettings_ClientSecret" --with-decryption --query "Parameter.Value" --output text)
 SqidsSettings_Alphabet=$(aws ssm get-parameter --name "SqidsSettings_Alphabet" --with-decryption --query "Parameter.Value" --output text)
 
-docker run -d -p 5184:5184 --name recordrack recordrack_image -e \
-    $ConnectionStrings $AppSettings $CloudinarySettings_ApiKey $CloudinarySettings_ApiSecret \
-    $CloudinarySettings_CloudName $SendgridSettings_ApiKey $SendgridSettings_FromEmail \ 
-    $SpotifySettings_ClientID $SpotifySettings_ClientSecret $SqidsSettings_Alphabet
+docker run -d -p 5184:5184 --name recordrack recordrack_image -e $ConnectionStrings $AppSettings $CloudinarySettings_ApiKey $CloudinarySettings_ApiSecret $CloudinarySettings_CloudName $SendgridSettings_ApiKey $SendgridSettings_FromEmail $SpotifySettings_ClientID $SpotifySettings_ClientSecret $SqidsSettings_Alphabet
