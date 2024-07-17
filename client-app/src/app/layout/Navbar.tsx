4e555e8f9f26ed413c3133f204656d31b6c9a0a7
Alex Taveras-Crespo <alexmanuelt@hotmail.com>
2024-07-17 11:03:51 -0400

    Adds active navigation link highlighting for logged in users

--- client-app/src/app/layout/Navbar.tsx
+++ client-app/src/app/layout/Navbar.tsx
@@ -11,6 +11,7 @@ import { NavLink } from "react-router-dom"
 import { useStore } from "../stores/store"
 import { observer } from "mobx-react-lite"
 import { BellIcon, LayoutGrid, Music, UserCircle, Info, LogIn, UserPlus } from "lucide-react"
+import RackNavLink from "./RackNavLink"
 
 // Define component icons
 export const Icons = {
@@ -34,7 +35,10 @@ function Navbar() {
       {/* Check userstore logged in method to determine if user is logged in */}
       {isLoggedIn
         ? 
-        <div className="header container py-6 flex flex-col items-center gap-6 md:flex-row">
+         /*
+          WHEN USER IS  SIGNED IN
+        */
+        <div className="header container py-4 flex flex-col items-center gap-2 md:flex-row">
           <div className=" w-full flex flex-row gap-2 justify-center items-center md:justify-start">
             <NavLink to={"/"}>
               <h1 className="font-semibold text-sm sm:text-base md:text-lg lg:text-xl">Record Rack</h1>
@@ -44,47 +48,71 @@ function Navbar() {
             <ModeToggle/>
           </div>
 
-          {/* Menu for signed in user */}
           <div className="w-full flex flex-row justify-between md:justify-end">
-            {/* Rack btn */}
-            <NavLink to={`${user?.userName}`}>
-                <Button variant="ghost" size="sm" className="flex flex-col w-12 justify-center md:w-24 md:h-16">
+
+            {/* Rack btn using custom rack nav link component */}
+            <RackNavLink to={`${user?.userName}`}>
+              {({ isActive }) => (
+                <Button variant="ghost" size="sm" className={
+                  `${isActive ? "text-neutral-950 border-neutral-950 dark:text-neutral-500 dark:border-neutral-500 font-extrabold rounded-b-none border-b-2" : ""} 
+                    flex flex-col w-12 justify-center md:w-24 md:h-16 hover:bg-transparent md:hover:bg-accent`
+                }>
                     <Icons.rack className="h-[1.2rem] w-[1.2rem] lg:h-[1.5rem] lg:w-[1.5rem] " />
                     <p className="text-[10px] xxxs:text-[11.5px] sm:text-sm lg:text-base">rack</p>
                 </Button>
-            </NavLink>
+              )}
+            </RackNavLink>
 
             {/* Search music btn */}
             <NavLink to={'search/music'}>
-                <Button variant="ghost" size="sm" className="flex flex-col w-12 justify-center md:w-24 md:h-16" >
+              {({ isActive }) => (
+                <Button variant="ghost" size="sm" className={
+                  `${isActive ? "text-neutral-950 border-neutral-950 dark:text-neutral-500 dark:border-neutral-500 font-extrabold rounded-b-none border-b-2" : ""} 
+                    flex flex-col w-12 justify-center md:w-24 md:h-16 hover:bg-transparent md:hover:bg-accent`
+                }>
                     <Icons.searchMusic className="h-[1.2rem] w-[1.2rem] lg:h-[1.5rem] lg:w-[1.5rem]" />
                     <p className="text-[10px] xxxs:text-[11.5px] sm:text-sm lg:text-base">search</p>
                 </Button>
+              )}
             </NavLink>
 
             {/* Search users btn */}
             <NavLink to={'search/users'}>
-                <Button variant="ghost" size="sm" className="flex flex-col w-14 justify-center items-center md:w-24 md:h-16" >
+              {({ isActive }) => (
+                <Button variant="ghost" size="sm" className={
+                  `${isActive ? "text-neutral-950 border-neutral-950 dark:text-neutral-500 dark:border-neutral-500 font-extrabold rounded-b-none border-b-2" : ""} 
+                    flex flex-col w-14 justify-center md:w-24 md:h-16 hover:bg-transparent md:hover:bg-accent`
+                }>                    
                     <Icons.searchUsers className="h-[1.2rem] w-[1.2rem] lg:h-[1.5rem] lg:w-[1.5rem]" />
                     <p className="text-[10px] xxxs:text-[11.5px] sm:text-sm lg:text-base">connect</p>
                 </Button>
+              )}
             </NavLink>
 
             {/* About btn */}
             <NavLink to={"/about"}>
-                <Button variant="ghost" size="sm" className="flex flex-col w-12 justify-center md:w-24 md:h-16" >
+              {({ isActive }) => (
+                <Button variant="ghost" size="sm" className={
+                  `${isActive ? "text-neutral-950 border-neutral-950 dark:text-neutral-500 dark:border-neutral-500 font-extrabold rounded-b-none border-b-2" : ""} 
+                    flex flex-col w-12 justify-center md:w-24 md:h-16 hover:bg-transparent md:hover:bg-accent`
+                }>
                     <Icons.about className="h-[1.2rem] w-[1.2rem] lg:h-[1.5rem] lg:w-[1.5rem]" />
                     <p className="text-[10px] xxxs:text-[11.5px] sm:text-sm lg:text-base">about</p>
                 </Button>
+              )}
             </NavLink>
 
-           
             {/* Notifications btn */}
             <NavLink to={`${user?.userName}/notifications`}>
-                <Button variant="ghost" size="sm" className="flex flex-col w-16 justify-center md:w-32 md:h-16" >
+              {({ isActive }) => (
+                <Button variant="ghost" size="sm" className={
+                  `${isActive ? "text-neutral-950 border-neutral-950 dark:text-neutral-500 dark:border-neutral-500 font-extrabold rounded-b-none border-b-2" : ""} 
+                    flex flex-col w-16 justify-center md:w-32 md:h-16 hover:bg-transparent md:hover:bg-accent`
+                }>
                     <Icons.notifications className="h-[1.2rem] w-[1.2rem] lg:h-[1.5rem] lg:w-[1.5rem]" />
                     <p className="text-[10px] xxxs:text-[11.5px] sm:text-sm lg:text-base">notifications</p>
                 </Button>
+              )}
             </NavLink>
 
           </div>
